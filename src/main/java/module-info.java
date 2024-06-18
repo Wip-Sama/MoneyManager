c51bc135aced25cbade273c1edfdb38ff0866d70
Orlando <sgro.orlando@gmail.com>
2024-06-18 16:06:38 +0200

    Test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module org.wip.moneymanager {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fluentui;
 
     opens org.wip.moneymanager to javafx.fxml;
     exports org.wip.moneymanager;
