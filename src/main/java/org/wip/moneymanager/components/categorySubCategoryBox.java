86a63a8c8ee32efd7b995684f501b381dbc1c76f
Luca Vangieri <lucavangieri@gmail.com>
2024-11-28 12:25:02 +0100

    prova

---
+++ src/main/java/org/wip/moneymanager/components/categorySubCategoryBox.java
@@ -0,0 +1,40 @@
+package org.wip.moneymanager.components;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+
+public class categorySubCategoryBox {
+
+    @FXML
+    private Label category;
+
+    @FXML
+    private ChoiceBox<?> categoryBox;
+
+    @FXML
+    private Label subCategory;
+
+    @FXML
+    private ChoiceBox<?> subCategoryBox;
+
+    public categorySubCategoryBox() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/wip/moneymanager/components/categorySubCategoryBox.fxml"));
+        fxmlLoader.setController(this);
+        fxmlLoader.setRoot(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize(){
+
+    }
+
+}
